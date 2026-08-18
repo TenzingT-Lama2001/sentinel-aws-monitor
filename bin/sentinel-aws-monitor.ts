bb8baea3d3f01843c7b4e23ce3db5816e0c84868
tenzing lama <tenzing.lama36@gmail.com>
2026-08-18 23:12:04 +1000

    chore: linting fix with eslint and prettier

--- bin/sentinel-aws-monitor.ts
+++ bin/sentinel-aws-monitor.ts
@@ -1,13 +1,19 @@
 #!/usr/bin/env node
-import 'dotenv/config';
-import * as cdk from 'aws-cdk-lib/core';
-import { SentinelAwsMonitorStack } from '../infra/sentinel-aws-monitor-stack';
+import "dotenv/config";
+import * as cdk from "aws-cdk-lib/core";
+import { SentinelAwsMonitorStack } from "../infra/sentinel-aws-monitor-stack";
 
 const app = new cdk.App();
-new SentinelAwsMonitorStack(app, 'SentinelAwsMonitorStack-Sydney', {
-  env: { account: process.env.AWS_ACCOUNT_ID, region: process.env.AWS_REGION_SYDNEY },
+new SentinelAwsMonitorStack(app, "SentinelAwsMonitorStack-Sydney", {
+  env: {
+    account: process.env.AWS_ACCOUNT_ID,
+    region: process.env.AWS_REGION_SYDNEY,
+  },
 });
 
-new SentinelAwsMonitorStack(app, 'SentinelAwsMonitorStack-Singapore', {
-  env: { account: process.env.AWS_ACCOUNT_ID, region: process.env.AWS_REGION_SINGAPORE },
+new SentinelAwsMonitorStack(app, "SentinelAwsMonitorStack-Singapore", {
+  env: {
+    account: process.env.AWS_ACCOUNT_ID,
+    region: process.env.AWS_REGION_SINGAPORE,
+  },
 });
